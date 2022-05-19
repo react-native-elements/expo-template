d7cfe8f632af214f7773ee4f99526efded3fffc5
Arpit Bhalla <55053424+arpitBhalla@users.noreply.github.com>
2022-05-19 17:25:39 +0530

    add themeProvider

--- templates/stable-ts/App.tsx
+++ templates/stable-ts/App.tsx
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { createTheme, ThemeProvider } from "@rneui/themed";
+import Component from "./components/MyComponent";
+
+const theme = createTheme({
+  lightColors: {},
+  darkColors: {},
+});
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <Component />
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ version.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const path = require("path");
+const { execSync } = require("child_process");
+
+const version = process.argv[2];
+const root = path.resolve(__dirname, "templates");
+
+fs.readdirSync(root).forEach((template) => {
+  const stable = template.includes("stable");
+  const manifestPath = path.resolve(root, template, "package.json");
+  const manifest = fs.readFileSync(manifestPath, "utf8");
+  const manifestJson = JSON.parse(manifest);
+  manifestJson.version = version + "." + template;
+  if (stable) {
+    manifestJson.dependencies["@rneui/base"] = version;
+    manifestJson.dependencies["@rneui/themed"] = version;
+  }
+  fs.writeFileSync(manifestPath, JSON.stringify(manifestJson, null, 2));
+});
